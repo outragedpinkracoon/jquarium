54a27777fb3245494ac39e210d5412610d186976
Glitch (aquarium) <none>
2018-09-19 10:15:12 +0000

    ✋⚡️ Checkpoint ./sketch.js:482818/991

--- sketch.js
+++ sketch.js
@@ -10,13 +10,16 @@
   resizeCanvas,
   windowWidth,
   windowHeight,
+  frameRate,
  */
+
+const FRAMERATE = 30
 
 class Fish {
   constructor() {
     this.location = { x: Math.random(), y: Math.random() }
     this.size = 10 + (Math.random() * 60)
-    this.speed = 10
+    this.speed = 15 + (Math.random() * 15)
     this.currentShoop = null
   }
   
@@ -36,18 +39,22 @@ class Fish {
   }
   
   updateShoop() {
-    if (!this.currentShoop) { return }
+    if (!this.currentShoop) {
+      return this.shoop()
+    }
     const windowXMe = this.location.x * windowWidth
     const windowYMe = this.location.y * windowHeight
     const windowXShoop = this.currentShoop.x * windowWidth
     const windowYShoop = this.currentShoop.y * windowHeight
     const delta = { x: windowXShoop - windowXMe, y: windowYShoop - windowYMe }
     const dist = Math.sqrt(Math.pow(delta.x, 2) + Math.pow(delta.y, 2))
-    const distToMove = Math.Min(dist, this.speed)
-    const reachesTarget = this.speed >= dist
+    const deltaNorm = { x: delta.x / dist, y: delta.y / dist }
+    const frameSpeed = this.speed / FRAMERATE
+    const distToMove = Math.min(dist, frameSpeed)
+    const reachesTarget = frameSpeed >= dist
     this.location = {
-      x: this.location.x + delta.x,
-      y: this.location.y + delta.y,
+      x: this.location.x + ((deltaNorm.x * distToMove) / windowWidth),
+      y: this.location.y + ((deltaNorm.y * distToMove) / windowHeight),
     }
     if (reachesTarget) { this.currentShoop = null }
   }
@@ -60,16 +67,14 @@ const fishies = [
 ]
 
 function setup() {
+  frameRate(FRAMERATE)
   createCanvas(windowWidth, windowHeight)
 }
 
 function draw() {
   drawBackground()
-  fishies.forEach(f => f.draw())
-}
-
-function update() {
   fishies.forEach(f => f.update())
+  fishies.forEach(f => f.draw())
 }
 
 function drawBackground() {
